a17b9617fe9fdc66ff99a87f82bdc71357c11ed3
SYABILLirfann <syabill.irfann08@gmail.com>
2026-09-16 01:19:45 +0800

    Complete Day 2 football squad analyzer

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello, world!")
+name = input("Whats is your name? ")
+print(f"welcome {name}!")
+
+goals = int(input("How many goals have you scored? "))
+
+assists = int(input("How many assists have you made? "))
+
+if goals >= 2 and assists >= 1:
+
+    print("Elite Performance")
+
+else:
+
+    print("Keep Improving")
+
+
+players = ["Haaland", "Foden", "Doku", "Rodri"]
+
+
+for player in players: 
+
+    print(f" Checking {player}!") 
+
+    if player is "Haaland":
+
+        print("STAR PLAYER FOUND")
+
+print(f"there are {len(players)} players in the squad")
