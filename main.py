5c2b1623e55f6d8e504bd1e76f04572452e00b06
SYABILLirfann <syabill.irfann08@gmail.com>
2026-09-24 19:59:50 +0800

    Complete Day 11 football player database

--- main.py
+++ main.py
@@ -1,106 +1,57 @@
-import requests
+import sqlite3
 
-def search_location(location):
-    search_parameters = {"name": location}
+connection = sqlite3.connect("player_database.db")
 
-    try:
-        response = requests.get(
-        "https://geocoding-api.open-meteo.com/v1/search",
-        params=search_parameters
-    )
-
-        if response.status_code == 200:
-            whole_data = response.json()
-
-            if "results" in whole_data:
-
-                location_data = {
-                                 "name": whole_data["results"][0]["name"],
-                                 "country": whole_data["results"][0]["country"],
-                                 "latitude": whole_data["results"][0]["latitude"],
-                                 "longitude": whole_data["results"][0]["longitude"],
-                                }
-                return location_data
-            
-            else:
-                return None
-        else:
-            return None
-    except requests.RequestException:
-        return None
+db_worker = connection.cursor()
 
 
+db_worker.execute("""
+CREATE TABLE IF NOT EXISTS players (
+    name TEXT,
+    position TEXT,
+    club TEXT,
+    goals INTEGER,
+    assists INTEGER
+    foot TEXT
+)
+""")
+#db_worker.execute(
+#    "ALTER TABLE players ADD COLUMN foot TEXT"
+#)
 
-def get_weather(location_data):
-    latitude = location_data["latitude"]
-    longitude = location_data["longitude"]
-
-    weather_parameters = {
-    "latitude": latitude,
-    "longitude": longitude,
-    "current": "temperature_2m,wind_speed_10m,weather_code"
-     } 
-
-
-    try:
-        weather_response = requests.get(
-        "https://api.open-meteo.com/v1/forecast",
-        params=weather_parameters
-    )
-        if weather_response.status_code == 200:
-            weather_data = weather_response.json()
-
-            temperature = weather_data["current"]["temperature_2m"]
-            wind_speed = weather_data["current"]["wind_speed_10m"]
-            weather_code = weather_data["current"]["weather_code"]
+name = input("Enter player name: ")
+position = input("Enter position: ")
+club = input("Enter club: ")
+goals = int(input("Enter goals: "))
+assists = int(input("Enter assists: "))
+foot = (input("Enter Foot: "))
 
 
-            weather_info = {
-                            "temperature": temperature,
-                            "wind_speed": wind_speed,
-                            "weather_code": weather_code
-                            }
+db_worker.execute(
+    "INSERT INTO players (name, position, club, goals, assists, foot) VALUES (?, ?, ?, ?, ?, ?)",
+  (name, position, club, goals, assists, foot)
+   )
 
-            return weather_info
+db_worker.execute(
+    "UPDATE players SET foot = ? WHERE name = ?",
+    ("left", "Messi")
+)
 
-        else:
-            return None
+connection.commit()
 
-    except requests.RequestException:
-        return None
+connection.commit()
 
+db_worker.execute("SELECT * FROM players")
 
-def build_weather_report(location_data, weather_info):
-    report = {
-    "destination": location_data["name"],
-    "country": location_data["country"],
-    "temperature": weather_info["temperature"],
-    "wind_speed": weather_info["wind_speed"],
-    "weather_code": weather_info["weather_code"],
-}
-
-    return report
+players = db_worker.fetchall()
 
 
+for player in players:
+    print(f"Name: {player[0]}")
+    print(f"Position: {player[1]}")
+    print(f"Club: {player[2]}")
+    print(f"Goals: {player[3]}")
+    print(f"Assists: {player[4]}")
+    print(f"Foot: {player[5]}")
 
-location = input("Enter a destination: ")
-
-location_data = search_location(location)
-
-if location_data:
-    weather_info = get_weather(location_data)
-
-    if weather_info:
-        report = build_weather_report(location_data, weather_info)
-
-        print(f"Destination: {report['destination']}")
-        print(f"Country: {report['country']}")
-        print(f"Temperature: {report['temperature']}")
-        print(f"Wind Speed: {report['wind_speed']}")
-        print(f"Weather Code: {report['weather_code']}")
-
-    else:
-        print("Weather could not be found")
-
-else:
-    print("Location could not be found")
+connection.close()
