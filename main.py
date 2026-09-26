a75f1d8c8a9174b67e19763f5ee6612d041b4027
SYABILLirfann <syabill.irfann08@gmail.com>
2026-09-27 00:07:49 +0800

    Complete Day 12 football squad manager

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 connection = sqlite3.connect("player_database.db")
-
 db_worker = connection.cursor()
 
 
@@ -11,47 +10,111 @@ CREATE TABLE IF NOT EXISTS players (
     position TEXT,
     club TEXT,
     goals INTEGER,
-    assists INTEGER
+    assists INTEGER,
     foot TEXT
 )
 """)
-#db_worker.execute(
-#    "ALTER TABLE players ADD COLUMN foot TEXT"
-#)
-
-name = input("Enter player name: ")
-position = input("Enter position: ")
-club = input("Enter club: ")
-goals = int(input("Enter goals: "))
-assists = int(input("Enter assists: "))
-foot = (input("Enter Foot: "))
 
 
-db_worker.execute(
+
+def add_player():
+    name = input("Enter player name: ")
+    position = input("Enter position: ")
+    club = input("Enter club: ")
+    goals = int(input("Enter player goals: "))
+    assists = int(input("Enter player assists: "))
+    foot = input("Enter player foot: ")
+
+    db_worker.execute(
     "INSERT INTO players (name, position, club, goals, assists, foot) VALUES (?, ?, ?, ?, ?, ?)",
-  (name, position, club, goals, assists, foot)
-   )
+    (name, position, club, goals, assists, foot)
+    )
 
-db_worker.execute(
-    "UPDATE players SET foot = ? WHERE name = ?",
-    ("left", "Messi")
-)
-
-connection.commit()
-
-connection.commit()
-
-db_worker.execute("SELECT * FROM players")
-
-players = db_worker.fetchall()
+    connection.commit()
 
 
-for player in players:
-    print(f"Name: {player[0]}")
-    print(f"Position: {player[1]}")
-    print(f"Club: {player[2]}")
-    print(f"Goals: {player[3]}")
-    print(f"Assists: {player[4]}")
-    print(f"Foot: {player[5]}")
+def view_players():
+    db_worker.execute("SELECT * FROM players")
+    players = db_worker.fetchall()
 
-connection.close()
+   
+    for player in players:
+      print(f"Name: {player[0]}")
+      print(f"Position: {player[1]}")
+      print(f"Club: {player[2]}")
+      print(f"Goals: {player[3]}")
+      print(f"Assists: {player[4]}")
+      print(f"Foot: {player[5]}")
+
+
+def search_player():
+    name = input("Enter player name: ")
+
+    db_worker.execute(
+        "SELECT * FROM players WHERE name = ?",
+        (name,)
+    )
+
+    player = db_worker.fetchone()
+
+    if player:
+       print(f"Name: {player[0]}")
+       print(f"Position: {player[1]}")
+       print(f"Club: {player[2]}")
+       print(f"Goals: {player[3]}")
+       print(f"Assists: {player[4]}")
+       print(f"Foot: {player[5]}")
+    else:
+       print("Player not found.")
+
+
+def update_player():
+    name = input("Enter player name: ")
+    new_club = input("Enter new club: ")
+
+    db_worker.execute(
+    "UPDATE players SET club = ? WHERE name = ?",
+    (new_club, name)
+    )
+
+    connection.commit()
+
+
+def delete_player():
+    name = input("Enter player name: ")
+   
+    db_worker.execute(
+        "DELETE FROM players WHERE name = ?",
+        (name,)
+        )
+
+    connection.commit()
+
+
+print("FOOTBALL SQUAD MANAGER")
+print("1. Add Player")
+print("2. View Player")
+print("3. Search player")
+print("4. Update Player")
+print("5. Delete Player")
+
+
+choice = input("Choose an option: ")
+
+if choice == "1":
+    add_player()
+
+elif choice == "2":
+     view_players()
+
+elif choice == "3":
+    search_player()
+
+elif choice == "4":
+    update_player()
+
+elif choice == "5":
+    delete_player()
+
+else:
+    print("Invalid option.")
